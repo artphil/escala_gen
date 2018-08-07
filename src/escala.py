a54263dc8777003e9cc92a7fdadfc892b318eb26
artphil <artphil.dev@gmail.com>
2018-08-07 09:04:13 -0300

    criando funcao le_dados

--- src/escala.py
+++ src/escala.py
@@ -4,8 +4,6 @@ from openpyxl.utils import column_index_from_string
 
 import numpy as np
 
-escala = []
-
 def imprime(tabela):
 	# Definindo planilha
 	wb = Workbook()
@@ -30,6 +28,7 @@ def imprime(tabela):
 	ws.merge_cells("A1:AG1")
 
 	ws['A1'].alignment = Alignment(horizontal='center', vertical='center')
+	ws['A1'].font = Font(bold=True)
 
 	for linha in ws['A2:AG15']:
 		ws.row_dimensions[linha[0].row].height = 20.0
@@ -51,60 +50,84 @@ def imprime(tabela):
 	wb.save(filename='escala_ASO1.xlsx')
 
 
-
-def gera_tabela():
-
-	estacao = "Central" #input("nome estacao: ")
-	escala.append(["Escala ASO1 - " + estacao, ""])
-
-	dia = 3 #int(input("dia da mudança: "))
-	lista_dias = []
-	lista_dias.append("Dias")
-	lista_dias.append("P")
-	for d in range(31):
-		lista_dias.append((dia+d-1)%31+1)
-	escala.append(lista_dias)
-
-	semana = ["D", "S", "T", "Q", "Q", "S", "S", "D"]
-	lista_sem = []
-	lista_sem.append(" ")
-	lista_sem.append(" ")
-	for d in range(31):
-		lista_sem.append(semana[d%7])
-	escala.append(lista_sem)
-
-	func_num = 4 #int(input("numero de funcionarios: "))
+def le_dados():
+	estacao = input("nome estacao: ")
+	dia = int(input("dia da mudança: "))
+	func_num = int(input("numero de funcionarios: "))
 	func_nomes = []
 	func_Ps = []
 	for i in range(func_num):
-		 func_nomes.append("Ana") #input("nome: "))
-		 func_Ps.append( 1 ) #int(input("P: ")))
+		func_nomes.append(input("nome: "))
+		func_Ps.append(int(input("P: ")))
 
-	distrib = preenche(4, 31)
+def gera_tabela():
+	escala = []
+	dias_mes = {
+	'Janeiro' : 31,
+	'Fevereiro' : 28,
+	'Marco' : 31,
+	'Abril' : 30,
+	'Maio' : 31,
+	'Junho' : 30,
+	'Julho' : 31,
+	'Agosto' : 31,
+	'Setembro' : 30,
+	'Outubro' : 31,
+	'Novembro' : 30,
+	'Dezembro' : 31
+	}
+	semana = ["D", "S", "T", "Q", "Q", "S", "S", "D"]
+
+
+	estacao = "Central"
+	dia = 3
+	mes = 'Agosto'
+	func_num = 4
+	func_nomes = ['Artphil', 'D.Maia', 'Waguim', 'Zeze']
+	func_Ps = [7, 8, 9, 4]
+
+
+	escala.append(["Escala ASO1 - " + estacao + " - " + mes, ""])
+
+	lista_dias = ["Dias", "P"]
+	for d in range(dias_mes[mes]):
+		lista_dias.append((dia+d-1)%dias_mes[mes]+1)
+	escala.append(lista_dias)
+
+	lista_sem = ["", ""]
+	for d in range(dias_mes[mes]):
+		lista_sem.append(semana[d%7])
+	escala.append(lista_sem)
+
+	distrib = preenche(func_num, dias_mes[mes])
 	postos = []
 	c = 0
 	for f in range(func_num):
 		p = []
-		for i in range(31):
+		p.append(func_nomes[f])
+		p.append(func_Ps[f])
+		for i in range(dias_mes[mes]):
 			if distrib[c][i] == 1:
 				p.append("F")
 			elif distrib[c][i] == 2:
 				p.append("B1")
 			elif distrib[c][i] == 3:
+				p.append("Q1")
+			elif distrib[c][i] == 4:
 				p.append("B2")
 			elif distrib[c][i] == 5:
-				p.append("Q1")
-			else:
 				p.append("Q2")
+			else:
+				p.append("")
 		c += 1
-		postos.append(p)
-
+		escala.append(p)
+	'''
 	for i in range(func_num):
 		linha = postos[i][:]
 		linha.insert(0,func_nomes[i])
 		linha.insert(1,func_Ps[i])
 		escala.append(linha)
-
+	'''
 	imprime(escala)
 
 	print ("Arquivo gerado no diretorio: ")
@@ -117,7 +140,7 @@ def preenche(func=4, dias=15):
 			if (i-j)%5 == 0:
 				dist_postos[i][j] = 1
 
-	postos = [2,3,5,7]
+	postos = [2,3,4,5]
 
 	for j in range(dias):
 		k = j%func;
