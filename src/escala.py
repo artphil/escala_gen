14b8d892878c594a25ffcae354e3e6ee51e095d9
artphil <artphil.dev@gmail.com>
2018-08-08 16:03:28 -0300

    testes de alocação mal sucedidos

--- src/escala.py
+++ src/escala.py
@@ -2,6 +2,7 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, Color, Alignment, PatternFill
 from openpyxl.utils import column_index_from_string
 
+import random
 import numpy as np
 
 def imprime(tabela):
@@ -53,10 +54,11 @@ def le_dados():
 	data['estacao'] = input("nome estacao: ")
 	data['dia_inicio'] = int(input("dia da mudança: "))
 	data['func_num'] = int(input("numero de funcionarios: "))
+	data['mes'] = input("mes: ")
 
 	func_nomes = []
 	func_Ps = []
-	for i in range(func_num):
+	for i in range(data['func_num']):
 		func_nomes.append(input("nome: "))
 		func_Ps.append(int(input("P: ")))
 
@@ -67,6 +69,7 @@ def le_dados():
 
 def gera_tabela():
 	escala = []
+
 	dias_mes = {
 	'Janeiro' : 31,
 	'Fevereiro' : 28,
@@ -81,16 +84,25 @@ def gera_tabela():
 	'Novembro' : 30,
 	'Dezembro' : 31
 	}
-	semana = ["D", "S", "T", "Q", "Q", "S", "S", "D"]
 
+	semana = ["D", "S", "T", "Q", "Q", "S", "S", "D"]
 
 	data = {}
 	data['estacao'] = "Central"
 	data['dia_inicio'] = 3
 	data['mes'] = 'Julho'
-	data['func_num'] = 4
-	data['func_nomes'] = ['Artphil', 'D.Maia', 'Waguim', 'Zeze']
-	data['func_Ps'] = [7, 8, 9, 4]
+	data['func_num'] = 6
+	data['func_nomes'] = ['Artphil', 'D.Maia', 'Waguim', 'Zeze', 'Tata', 'Tretas']
+	data['func_Ps'] = [1, 4, 10, 2, 5, 11]
+
+	data['folgas'] = [
+	[0,5,10,11,15,20,25,26],
+	[1,5,10,11,16,20,25,26],
+	[0,4,10,11,15,20,24,25],
+	[2,6,12,16,21,22,27],
+	[2,6,12,15,21,22,27],
+	[2,5,13,16,21,22,27]]
+	data['postos'] = [2,1]
 
 	# data = le_dados()
 
@@ -106,7 +118,7 @@ def gera_tabela():
 		lista_sem.append(semana[d%7])
 	escala.append(lista_sem)
 
-	distrib = preenche(data['func_num'], dias_mes[data['mes']])
+	distrib = preenche(data['func_num'], dias_mes[data['mes']], data['postos'], data['folgas'])
 	postos = []
 	c = 0
 	for f in range(data['func_num']):
@@ -133,25 +145,94 @@ def gera_tabela():
 
 	print ("Arquivo gerado no diretorio: ")
 
-def preenche(func=4, dias=15):
+def preenche(func=6, dias=15, postos_num=[2,1], folgas=[[0,4,7,8,11,14],[1,4,7,8,11,14],[0,4,7,8,11,14],[2,3,6,9,12],[2,3,6,9,12],[2,3,6,9,12]]):
 	dist_postos = np.zeros((func,dias))
 
-	for i in range(func):
-		for j in range(dias):
-			if (i-j)%5 == 0:
-				dist_postos[i][j] = 1
+	func_fixos = int(func/2)
+	postos_trab = np.zeros((func,func_fixos))
 
-	postos = [2,3,4,5]
+	f=0
+	for lista in folgas:
+		for d in lista:
+			dist_postos[f][d] = 1
+		f += 1
 
+	postos = []
+	m = 2
+	for qtd in postos_num:
+		for i in range(qtd):
+			postos.append(i*2 + m)
+		m += 1
+
+	postos.sort()
+	print (postos)
+	
 	for j in range(dias):
-		k = j%func;
-		for i in range(func):
-			if dist_postos[i][j] != 1:
-				dist_postos[i][j] = postos[k]
-				k = (k+1)%func
+		k = j%func_fixos;
+		for i in range(func_fixos):
+			# print(i,j,k)
+			if dist_postos[i][j] == 1:
+				i += func_fixos
+			dist_postos[i][j] = postos[k]
+			k = (k+1)%func_fixos
+	'''
+	for d in range(dias):
+		postos_ocp = np.zeros(func_fixos)
+		num_postos = func_fixos
+		f = random.randint(0,func_fixos)
+		while num_postos > 0:
+			print(f)
+			if dist_postos[f][d] != 0:
+				if dist_postos[f][d] == 1:
+					f = (f+func_fixos)%func
+				else:
+					f = (f+1)%func_fixos
+			else:
+				p_min=999
+				p_i = 0
+				for i in range(func_fixos):
+					if postos_ocp[i] == 0 and postos_trab[f][i] < p_min:
+						p_min = postos_trab[f][i]
+						p_i = i
+				print(f, p_i)
+				postos_ocp[p_i] = 1
+				postos_trab[f][p_i] += 1
+				dist_postos[f][d] = postos[p_i]
+				num_postos -= 1
+				# f = (f+1)%func_fixos
+	''' '''
+	for d in range(dias):
+		fix = []
+		res = []
+		for f in range(func_fixos):
+			if dist_postos[f][d] == 1:
+				res.append(f+func_fixos)
+			else:
+				fix.append(f)
 
+		for p in range(func_fixos):
+			f_min=999
+			f_i = 0
+			for i in range(func_fixos):
+				if postos_trab[i][p] < f_min:
+					f_min = postos_trab[i][p]
+					f_i = i
+
+			postos_trab[f_i][p] += 1
+			postos_trab[f_i += func_fixos][p] += 1
+
+			if dist_postos[f_i][d] == 1:
+				f_i += func_fixos
+
+			print(f_i, p)
+
+			dist_postos[f_i][d] = postos[p]
+	'''
 	print (dist_postos)
 	return dist_postos
+
+def aloca():
+	pass
 
 
 gera_tabela()
