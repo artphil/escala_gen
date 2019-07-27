1c1761d5cf2de0ba17ddc17085244842d8a48211
artphil <artphil.dev@gmail.com>
2019-07-27 12:44:44 -0300

    gravando sequencias de alocacao

--- src/prob.py
+++ src/prob.py
@@ -28,16 +28,35 @@ def gerador(final, vetor, vistos, aux):
 # Lista de combinacoes de um vetor
 def gera_p(vetor):
 	global p
-	tam = len(vetor)
-	vistos = np.zeros(tam)
-	aux = []
-	final = []
+	v_name = ""
+	for i in vetor:
+		v_name += str(i)
+	
+	print(v_name)
+	f_name = "data/seqs/"+v_name
+	print(f_name)
+	try:
+		final = []
+		# recupera uma sequencia gravada
+		file = open(f_name,"r")
+		for line in file.readlines():
+			final.append([int(x) for x in line.split(',')])
+	except:
+		tam = len(vetor)
+		vistos = np.zeros(tam)
+		aux = []
+		final = []
 
-	# Gera combinacoes
-	gerador(final, vetor, vistos, aux)
+		# Gera combinacoes
+		gerador(final, vetor, vistos, aux)
 
-	# Embaralha resultado
-	shuffle(final)
+		# Embaralha resultado
+		shuffle(final)
 
+		# Grava a sequencia
+		file = open(f_name,"w")
+		for line in final:
+			file.write(str(line)[1:-1]+"\n")
+		
 	p = final
 	return final
